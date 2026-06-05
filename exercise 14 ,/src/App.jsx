6412862a5ed8b516920dc64dcb963d4123183c95
camaar1918 <engcamaaromar@gmail.com>
2026-06-05 16:07:33 +0300

    exercise 1$

---
+++ exercise 14 ,/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './app.css'
+
+import Userlis from './userlis'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+
+<div  className='to'>
+  <h1 className='h1'>My To Do List</h1>
+  <input className='input' type="text" />
+  <button className='button'>Add</button>
+
+
+
+
+
+</div>
+   
+
+     
+    </>
+  )
+}
+
+export default App
